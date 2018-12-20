1e0681d1d0ed4c02e47eeffc4a9e91d24195a5b5
Muhammad Zaman <mzamann1@users.noreply.github.com>
2018-12-20 15:06:20 +0500

    Initial commit

---
+++ RazorPages_Movie/Data/RazorPages_MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPages_Movie.Models
+{
+    public class RazorPages_MovieContext : DbContext
+    {
+        public RazorPages_MovieContext (DbContextOptions<RazorPages_MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
---
+++ RazorPages_Movie/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPages_Movie.Models;
+
+namespace RazorPages_Movie.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPages_MovieContext _context;
+
+        public CreateModel(RazorPages_MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
